f8b2788f89da15557e37d68b9cf0bf24a0fdeacf
fkakd1205 <fkakd0102@naver.com>
2022-06-02 19:41:29 +0900

    REFACTOR : 인증토큰 발급 요청은 서버에서 실시하자

--- src/api/socialLoginDataConnect.js
+++ src/api/socialLoginDataConnect.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 const API_SERVER_ADDRESS = process.env.REACT_APP_API_HOST;
 
-const socialLoginDataConnect = () =>{
-    return{
+const socialLoginDataConnect = () => {
+    return {
         naverLogin: async function (token) {
             return await axios.get(`${API_SERVER_ADDRESS}/api/v1/social-login/naver`, {
                 params: {
@@ -12,20 +12,12 @@ const socialLoginDataConnect = () =>{
                 withCredentials: true
             })
         },
-        kakaoAuth: async function (params) {
-            return await axios.post(`https://kauth.kakao.com/oauth/token`, params);
-        },
-        kakaoLogin: async function(token) {
+        kakaoLogin: async function (token) {
             return await axios.get(`${API_SERVER_ADDRESS}/api/v1/social-login/kakao`, {
                 params: {
                     token
                 },
                 withCredentials: true
-            })
-        },
-        googleAuth: async function (params) {
-            return await axios.post(`https://oauth2.googleapis.com/token?` + params, {
-                withCredentials:true
             })
         },
         googleLogin: async function (token) {
@@ -34,11 +26,6 @@ const socialLoginDataConnect = () =>{
                     token
                 },
                 withCredentials: true
-            })
-        },
-        facebookAuth: async function (params) {
-            return await axios.get(`https://graph.facebook.com/v14.0/oauth/access_token?` + params, {
-                // withCredentials: true
             })
         },
         facebookLogin: async function (token) {
@@ -52,6 +39,6 @@ const socialLoginDataConnect = () =>{
     }
 }
 
-export{
+export {
     socialLoginDataConnect
 }
--- src/component/login/LoginMain.js
+++ src/component/login/LoginMain.js
@@ -106,20 +106,18 @@ const Container = styled.div`
 const REDIRECT_URI = "http://localhost:3000/home";
 
 const NAVER_REST_API_KEY = process.env.REACT_APP_NAVER_REST_API_KEY;
+const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_REST_API_KEY}&redirect_uri=${REDIRECT_URI + `?platform=naver`}&response_type=code&state=hihi`;
 
 const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
 const KAKAO_NEEDS_AGREMENTS = "account_email,profile_nickname";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI + `?platform=kakao`}&response_type=code&scope=${KAKAO_NEEDS_AGREMENTS}`;
-const KAKAO_REST_API_SECRET_KEY = process.env.REACT_APP_KAKAO_REST_API_SECRET_KEY;
 
 const GOOGLE_REST_API_KEY = process.env.REACT_APP_GOOGLE_REST_API_KEY;
 const GOOGLE_NEEDS_AGREMENTS = "email profile";
 const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_REST_API_KEY}&redirect_uri=${REDIRECT_URI + `?platform=google`}&response_type=code&scope=${GOOGLE_NEEDS_AGREMENTS}&access_type=offline`;
-const GOOGLE_REST_API_SECRET_KEY = process.env.REACT_APP_GOOGLE_REST_API_SECRET_KEY;
 
 const FACEBOOK_REST_API_KEY = process.env.REACT_APP_FACEBOOK_REST_API_KEY;
 const FACEBOOK_AUTH_URL = `https://www.facebook.com/v14.0/dialog/oauth?client_id=${FACEBOOK_REST_API_KEY}&redirect_uri=${REDIRECT_URI + `?platform=facebook`}&state=hihi`;
-const FACEBOOK_REST_API_SECRET_KEY = process.env.REACT_APP_FACEBOOK_REST_API_SECRET_KEY;
 
 const LoginMain = () => {
     const [loginData, dispatchLoginData] = useReducer(loginDataReducer, initialLoginDataReducer);
@@ -129,43 +127,9 @@ const LoginMain = () => {
     const query = qs.parse(location.search);
 
     useEffect(() => {
-        const naverLogin = new window.naver.LoginWithNaverId({
-            clientId: NAVER_REST_API_KEY,
-            callbackUrl: "http://localhost:3000/home?platform=naver",
-            isPopup: true,
-            loginButton: {color:"green", type:1, height:40},
-            callbackHandle: true
-        });
-        naverLogin.init();
-    }, []);
-
-    useEffect(() => {
         async function naverAuth() {
-            const hash = location.hash;
-            if(!hash) return;
-            if(!query || !query.platform) return;
-            if(query.platform !== 'naver') return;
-            
-            const token = location.hash.split('=')[1].split('&')[0];
-            if(!token) return;
-
-            await __reqNaverLogin(token);
-
-            // hash값 제거
-            navigate(qs.stringifyUrl({
-                url: location.pathname,
-                query: ''
-            }),
-            {
-                replace: true
-            })
-        }
-
-        async function kakaoAuth() {
-            if(!query || !query.platform || !query.code) return;
-            if(query.platform !== 'kakao') return;
             const auth_token = query.code;
-            
+
             navigate(qs.stringifyUrl({
                 url: location.pathname,
                 query: ''
@@ -173,27 +137,23 @@ const LoginMain = () => {
                 replace: true
             });
 
-            const body = qs.stringify({
-                grant_type: "authorization_code",
-                client_id: KAKAO_REST_API_KEY,
-                redirect_uri: "http://localhost:3000/home?platform=kakao",
-                code: auth_token,
-                client_secret: KAKAO_REST_API_SECRET_KEY
+            await __reqNaverLogin(auth_token);
+        }
+
+        async function kakaoAuth() {
+            const auth_token = query.code;
+            
+            navigate(qs.stringifyUrl({
+                url: location.pathname,
+                query: ''
+            }), {
+                replace: true
             });
 
-            // 인증토큰발급
-            window.Kakao.init(KAKAO_REST_API_KEY);
-            await __reqKakaoAuth(body);
-            
-            // 카카오 로그인
-            const ac_token = window.Kakao.Auth.getAccessToken();
-            await __reqKakaoLogin(ac_token);
-            return;
+            await __reqKakaoLogin(auth_token);
         }
 
         async function googleAuth() {
-            if (!query || !query.platform || !query.code) return;
-            if (query.platform !== 'google') return;
             const auth_token = query.code;
 
             navigate(qs.stringifyUrl({
@@ -203,21 +163,10 @@ const LoginMain = () => {
                 replace: true
             });
 
-            const params = qs.stringify({
-                grant_type: "authorization_code",
-                client_id: GOOGLE_REST_API_KEY,
-                redirect_uri: "http://localhost:3000/home?platform=google",
-                code: auth_token,
-                client_secret: GOOGLE_REST_API_SECRET_KEY,
-            });
-
-            let ac_token = await __reqGoogleAuth(params);
-            await __reqGoogleLogin(ac_token);
+            await __reqGoogleLogin(auth_token);
         }
 
         async function facebookAuth() {
-            if (!query || !query.platform || !query.code) return;
-            if (query.platform !== 'facebook') return;
             const auth_token = query.code;
 
             navigate(qs.stringifyUrl({
@@ -227,27 +176,42 @@ const LoginMain = () => {
                 replace: true
             });
 
-            const params = qs.stringify({
-                client_id: FACEBOOK_REST_API_KEY,
-                redirect_uri: "http://localhost:3000/home?platform=facebook",
-                code: auth_token,
-                client_secret: FACEBOOK_REST_API_SECRET_KEY,
-            });
-
-            let ac_token = await __reqFacebookAuth(params);
-            await __reqFacebookLogin(ac_token);
+            await __reqFacebookLogin(auth_token);
         }
 
-        naverAuth();
-        kakaoAuth();
-        googleAuth();
-        facebookAuth();
+        if(!query || !query.platform || !query.code) return;
+        switch(query.platform) {
+            case 'naver':
+                naverAuth();
+                break;
+            case 'kakao':
+                kakaoAuth();
+                break;
+            case 'google':
+                googleAuth();
+                break;
+            case 'facebook':
+                facebookAuth();
+                break;
+            default:
+                return;
+        }
     }, []);
 
     const _onSubmit_postLogin = async (e) => {
         e.preventDefault();
 
         await __reqPostLogin();
+    }
+
+    const onChangeLoginData = (e) => {
+        dispatchLoginData({
+            type: 'CHANGE_DATA',
+            payload: {
+                name: e.target.name,
+                value: e.target.value
+            }
+        });
     }
 
     // 피아르 로그인
@@ -265,37 +229,11 @@ const LoginMain = () => {
             })
     }
 
-    const onChangeLoginData = (e) => {
-        dispatchLoginData({
-            type: 'CHANGE_DATA',
-            payload: {
-                name: e.target.name,
-                value: e.target.value
-            }
-        });
-    }
-
     // 네이버 로그인
     const __reqNaverLogin = async (token) => {
         await socialLoginDataConnect().naverLogin(token)
             .then(res => {
                 navigate('/');
-            })
-            .catch(err => {
-                let res = err?.response;
-                if (res?.status === 500) {
-                    alert('undefined error.');
-                    return;
-                }
-                alert(res?.data?.memo);
-            })
-    }
-
-    // 카카오 인증토큰 발급
-    const __reqKakaoAuth = async (params) => {
-        await socialLoginDataConnect().kakaoAuth(params)
-            .then(res => {
-                window.Kakao.Auth.setAccessToken(res.data.access_token);
             })
             .catch(err => {
                 let res = err?.response;
@@ -323,7 +261,7 @@ const LoginMain = () => {
             })
     }
 
-    // 카카오 인증토큰 발급
+    // 구글 인증토큰 발급
     const __reqGoogleAuth = async (params) => {
         return await socialLoginDataConnect().googleAuth(params)
             .then(res => {
@@ -339,6 +277,7 @@ const LoginMain = () => {
             })
     }
 
+    // 구글 로그인
     const __reqGoogleLogin = async (token) => {
         await socialLoginDataConnect().googleLogin(token)
             .then(res => {
@@ -354,21 +293,7 @@ const LoginMain = () => {
             })
     }
 
-    const __reqFacebookAuth = async (params) => {
-        return await socialLoginDataConnect().facebookAuth(params)
-            .then(res => {
-                return res.data.access_token;
-            })
-            .catch(err => {
-                let res = err?.response;
-                if (res?.status === 500) {
-                    alert('undefined error.');
-                    return;
-                }
-                alert(res?.data?.memo);
-            })
-    }
-
+    // 페이스북 로그인
     const __reqFacebookLogin = async (token) => {
         await socialLoginDataConnect().facebookLogin(token)
             .then(res => {
@@ -390,10 +315,10 @@ const LoginMain = () => {
                 <div className='title'>Login</div>
                 <div className='flex-box'>
                     <div>
-                        <input type='text' name='username' onChange={(e) => onChangeLoginData(e)} value={loginData?.username || ''}></input>
+                        <input type='text' name='username' onChange={(e) => onChangeLoginData(e)} value={loginData?.username || ''} required></input>
                     </div>
                     <div>
-                        <input type='password' name='password' onChange={(e) => onChangeLoginData(e)} value={loginData?.password || ''}></input>
+                        <input type='password' name='password' onChange={(e) => onChangeLoginData(e)} value={loginData?.password || ''} required></input>
                     </div>
                     <div>
                         <button type='submit'>로그인</button>
@@ -408,16 +333,16 @@ const LoginMain = () => {
                 <div className='social-login-title'>간편로그인</div>
                 <div className='social-login-wrapper'>
                     <div className='social-button'>
-                        <div id='naverIdLogin' className='naver-login-box'></div>
+                        <a href={NAVER_AUTH_URL}><img src="/assets/naver_login.png" width='40'/></a>
                     </div>
                     <div className='social-button'>
                         <a href={KAKAO_AUTH_URL} target='_brank'><img src="/assets/kakao_login.png" width='40' /></a>
                     </div>
                     <div className='social-button'>
-                        <a className='login-logo' href={GOOGLE_AUTH_URL} target='_brank'><img src="/assets/google_login.png" width='40' /></a>
+                        <a href={GOOGLE_AUTH_URL} target='_brank'><img src="/assets/google_login.png" width='40' /></a>
                     </div>
                     <div className='social-button'>
-                        <a className='login-logo' href={FACEBOOK_AUTH_URL} target='_brank'><img src="/assets/facebook_login.png" width='40' /></a>
+                        <a href={FACEBOOK_AUTH_URL} target='_brank'><img src="/assets/facebook_login.png" width='40' /></a>
                     </div>
                 </div>
             </div>
